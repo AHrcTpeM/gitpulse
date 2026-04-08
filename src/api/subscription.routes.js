e7839d91d3e63199167d23b3262bd33e4398f400
ahrctpem <igorpopkov86@gmail.com>
2026-04-08 13:09:14 +0300

    feat: implement request validation using express-validator

--- src/api/subscription.routes.js
+++ src/api/subscription.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const subscriptionController = require('./subscription.controller');
+const { 
+  validateSubscription, 
+  validateToken, 
+  validateGetSubscriptions 
+} = require('./subscription.validator');
 
-router.post('/subscribe', subscriptionController.subscribe);
-router.get('/confirm/:token', subscriptionController.confirmSubscription);
-router.get('/unsubscribe/:token', subscriptionController.unsubscribe);
-router.get('/subscriptions', subscriptionController.getSubscriptions);
+router.post('/subscribe', validateSubscription, subscriptionController.subscribe);
+router.get('/confirm/:token', validateToken, subscriptionController.confirmSubscription);
+router.get('/unsubscribe/:token', validateToken, subscriptionController.unsubscribe);
+router.get('/subscriptions', validateGetSubscriptions, subscriptionController.getSubscriptions);
 
 module.exports = router;
